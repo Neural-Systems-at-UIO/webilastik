366d2d38011da43ee81675b982ac83817f018fe3
Arda Balkir <abalkir2014@gmail.com>
2025-06-20 11:04:59 +0000

    Env additions

---
+++ mpi_setup.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install build-essential
+sudo apt install libopenmpi-dev openmpi-bin
+# libopenmpi-dev is needed for mpi4py 
+sudo apt-get install nodejs npm
+curl -fsSL https://deno.land/install.sh | sh
