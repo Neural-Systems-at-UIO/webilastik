d7bcb1a3d65ac24c523edbd069c9e399341256d3
Arda Balkir <abalkir2014@gmail.com>
2025-06-21 22:58:42 +0000

    Added mamba setup

--- mpi_setup.sh
+++ mpi_setup.sh
@@ -1,6 +1,14 @@
 sudo apt update
+
 sudo apt install build-essential
-sudo apt install libopenmpi-dev openmpi-bin
-# libopenmpi-dev is needed for mpi4py 
+sudo apt install libopenmpi-dev openmpi-bin # libopenmpi-dev is needed for mpi4py 
+
 sudo apt-get install nodejs npm
 curl -fsSL https://deno.land/install.sh | sh
+
+echo "Setting up the mamba environment via miniforge"
+
+wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh"
+bash Miniforge3-Linux-x86_64.sh
+
+mamba env create -f environment.yml # Includes everything for webilastik env now
